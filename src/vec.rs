6c0f8681008e7ec164feab0ed6205068b837e8c3
Adam Sadek <adam.sadekk@gmail.com>
2026-07-09 22:36:56 +0100

    fix major bug with print() api; add new tests

--- src/vec.rs
+++ src/vec.rs
@@ -42,7 +42,8 @@ impl<T> RawVec<T> {
             if i != 0 {
                 print!(", ");
             }
-            unsafe { print!("{:?}", self.start.add(i).read()) };
+            let element_ptr = unsafe { &*self.start.add(i) };
+            print!("{:?}",  element_ptr);
         }
         println!("]");
     }
@@ -57,23 +58,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_push_exact_vec_size() {
+    fn test_push_u32_exact_vec_size() {
         const VEC_SIZE: usize = 10;
         let mut vec: RawVec<u32> = RawVec::new(VEC_SIZE);
 
         for i in 1..=VEC_SIZE as u32 {
             vec.push(i);
         }
+        vec.print();
         assert_eq!(vec.len(), VEC_SIZE, "expected vector size to be {VEC_SIZE}");
     }
 
     #[test]
-    fn test_push_one_less_than_size() {
+    fn test_push_usize_one_less_than_size() {
         const VEC_SIZE: usize = 10;
         const VEC_SIZE_MINUS_ONE: usize = VEC_SIZE - 1;
-        let mut vec: RawVec<u32> = RawVec::new(VEC_SIZE);
+        let mut vec: RawVec<usize> = RawVec::new(VEC_SIZE);
 
-        for i in 1..=VEC_SIZE_MINUS_ONE as u32 {
+        for i in 1..=VEC_SIZE_MINUS_ONE {
             vec.push(i);
         }
         assert_eq!(vec.len(), VEC_SIZE_MINUS_ONE);
@@ -84,6 +86,31 @@ mod tests {
         );
     }
 
+    #[test]
+    fn test_push_string_exact_vec_size() {
+        const VEC_SIZE: usize = 10;
+        let mut vec: RawVec<String> = RawVec::new(VEC_SIZE);
+
+        for _ in 1..=VEC_SIZE {
+            vec.push("test".to_string());
+        }
+        vec.print();
+        assert_eq!(vec.len(), VEC_SIZE, "expected vector size to be {VEC_SIZE}");
+    }
+
+    #[test]
+    fn test_push_varied_strings() {
+        let inputs = ["", "a", "hello world", "🦀", "line1\nline2", "  spaces  "];
+        let mut vec: RawVec<String> = RawVec::new(inputs.len());
+
+        for s in inputs {
+            vec.push(s.to_string());
+        }
+        vec.print();
+        assert_eq!(vec.len(), inputs.len());
+    }
+
+    
     #[test]
     #[should_panic(expected = "RawVec capacity exceeded")]
     fn test_push_fails_on_out_of_bounds() {
